751ea003242f51abf7baa4c1c43d282c34001973
LFDM <1986gh@gmail.com>
2015-12-27 09:59:13 +0100

    fix(tokenizer) More spec scoping

--- processing/tokenizer/spec.js
+++ processing/tokenizer/spec.js
@@ -1,26 +1,24 @@
 import service from '.';
 
-describe('tokenizer', () => {
-  describe('tokenize()', () => {
-    it('tokenizes a text', () => {
-      const text = 'This is a test text.';
-      const expected = ['This', 'is', 'a', 'test', 'text', '.'];
-      const actual = service.tokenize(text);
-      expect(actual).to.deep.equal(expected);
-    });
+describe('tokenize()', () => {
+  it('tokenizes a text', () => {
+    const text = 'This is a test text.';
+    const expected = ['This', 'is', 'a', 'test', 'text', '.'];
+    const actual = service.tokenize(text);
+    expect(actual).to.deep.equal(expected);
+  });
 
-    it('does this in a treebanky style', () => {
-      const text = 'This isn\'t a test text.';
-      const expected = ['This', 'is', 'n\'t', 'a', 'test', 'text', '.'];
-      const actual = service.tokenize(text);
-      expect(actual).to.deep.equal(expected);
-    });
+  it('does this in a treebanky style', () => {
+    const text = 'This isn\'t a test text.';
+    const expected = ['This', 'is', 'n\'t', 'a', 'test', 'text', '.'];
+    const actual = service.tokenize(text);
+    expect(actual).to.deep.equal(expected);
+  });
 
-    it('deals with multiple sentences as well', () => {
-      const text = 'A sentence. And another.';
-      const expected = ['A', 'sentence', '.', 'And', 'another', '.'];
-      const actual = service.tokenize(text);
-      expect(actual).to.deep.equal(expected);
-    });
+  it('deals with multiple sentences as well', () => {
+    const text = 'A sentence. And another.';
+    const expected = ['A', 'sentence', '.', 'And', 'another', '.'];
+    const actual = service.tokenize(text);
+    expect(actual).to.deep.equal(expected);
   });
 });
