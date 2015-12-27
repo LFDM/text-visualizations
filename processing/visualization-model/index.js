0aaa83f5c6610d7650d812d7390a3d087abbcbed
LFDM <1986gh@gmail.com>
2015-12-27 01:32:04 +0100

    impr(visualization-model) Add a delegator

--- processing/visualization-model/index.js
+++ processing/visualization-model/index.js
@@ -14,26 +14,13 @@ export default class VisualizationModel {
 
     this._lists = {};
     this._map = {};
-  }
 
-  forEach(fn) {
-    return this.sources.forEach(fn);
-  }
-
-  map(fn) {
-    return this.sources.map(fn);
-  }
-
-  reduce(fn, mem) {
-    return this.sources.reduce(fn, mem);
+    delegate.call(this, 'sources', ['forEach', 'map', 'reduce']);
+    delegate.call(this, '_normalizer', ['normalize']);
   }
 
   tokenize(opts = {}) {
     return tokenize(this, opts);
-  }
-
-  normalize(token) {
-    return this._normalizer.normalize(token);
   }
 
   getAllTokens(opts = {}) {
@@ -85,4 +72,13 @@ function computeCached(instance, path, fn) {
     set(instance, path, fn());
   }
   return get(instance, path);
+}
+
+// this will be a decorator once babel 6 supports them again
+function delegate(target, delegators) {
+  delegators.forEach((delegator) => {
+    this[delegator] = function(...args) {
+      return this[target][delegator](...args);
+    };
+  });
 }
